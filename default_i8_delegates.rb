3e7f8801c78c2acc81402efb91813dd385855c1f
Alexander Cairns <alexandercairns@discoverygarden.ca>
2026-03-04 16:21:44 -0400

    feat: site id validation

--- default_i8_delegates.rb
+++ default_i8_delegates.rb
@@ -57,6 +57,12 @@ class CustomDelegate
     end
   end
 
+  # check if site_id is valid when validation regex is set
+  def _site_id_valid?
+    re = /#{$info.fetch('site_id_regex','^[a-zA-Z][a-zA-Z0-9_-]*$')}/
+    return re.match?(_site_id)
+  end
+
   # Build up the full URL to the resource.
   def _resource
     if _site_id and _suffix
@@ -70,6 +76,9 @@ class CustomDelegate
   # Override; allow the passing of additional headers for auth.
   def httpsource_resource_info(options = {})
     if _site_id
+      unless _site_id_valid?
+        return nil
+      end
       url = _resource
 
       $logger.debug("Site ID '#{_site_id}' resolved to '#{url}'.")
@@ -163,6 +172,10 @@ class CustomDelegate
   # Override; handle I8 resource auth.
   def pre_authorize(options = {})
     # If...
+    if _site_id and not _site_id_valid?
+      $logger.debug("Site ID '#{_site_id}' is invalid.")
+      return false
+    end
     if _resource
       # ... we have something that appears to be an I8 resource, enforce auth...
       $sites_cache.limit = ($info['sitemap'].length + $info['sites_cache_size'])
